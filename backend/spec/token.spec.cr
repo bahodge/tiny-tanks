4388addef8de323dc39a67d51721aa3ea3f14722
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2022-02-08 22:49:35 -0600

    Added tests for token

---
+++ backend/spec/token.spec.cr
@@ -0,0 +1,100 @@
+require "uuid/json"
+require "base64"
+require "./spec_helper.cr"
+require "../src/token.cr"
+
+describe Token do
+  describe "constructor" do
+    it "initializes the token with passed arguments" do
+      token = Token.new(id: UUID.random, expires_at: Time.utc)
+      token.should_not be_nil
+      token.id.should be_a(UUID)
+      token.expires_at.should be_a(Time)
+    end
+
+    it "has defaults" do
+      token = Token.new
+      token.should_not be_nil
+      token.id.should be_a(UUID)
+      token.expires_at.should be_a(Time)
+    end
+  end
+
+  describe "#encode" do
+    it "returns a string" do
+      token = Token.new
+      token.encode.should be_a(String)
+    end
+  end
+
+  describe "#is_valid" do
+    it "returns true when @expires_at is in the future" do
+      token = Token.new
+      token.is_valid?.should eq(true)
+    end
+
+    it "returns false when @expires_at is in the past" do
+      token = Token.new expires_at: Time.utc.shift days: -1
+      token.is_valid?.should eq(false)
+    end
+  end
+
+  describe "#to_json" do
+    it "serializes to json string" do
+      json = Token.new.to_json
+
+      # Assert
+      json.should be_a(String)
+      token = Token.from_json(json)
+
+      token.id.should be_a(UUID)
+      token.expires_at.should be_a(Time)
+    end
+  end
+
+  describe "#decode" do
+    it "returns a string" do
+      token = Token.new
+      encoded = Base64.strict_encode("cookies")
+      decoded = token.decode(encoded)
+
+      decoded.should eq("cookies")
+    end
+  end
+
+  describe ".decode" do
+    it "returns a string" do
+      encoded = Base64.strict_encode("cookies")
+      decoded = Token.decode(encoded)
+
+      decoded.should eq("cookies")
+    end
+  end
+
+  describe ".from_json" do
+    it "can be derrived from json" do
+      json = Token.new.to_json
+
+      result = Token.from_json json
+      result.should be_a(Token)
+    end
+  end
+
+  describe ".from_encoded" do
+    it "returns a new token" do
+      token = Token.new
+      encoded_token = token.encode
+
+      result = Token.from_encoded encoded_token
+      result.should be_a(Token)
+      result.id.should eq(token.id)
+      result.expires_at.year.should eq(token.expires_at.year)
+      result.expires_at.month.should eq(token.expires_at.month)
+      result.expires_at.day.should eq(token.expires_at.day)
+      result.expires_at.hour.should eq(token.expires_at.hour)
+      result.expires_at.minute.should eq(token.expires_at.minute)
+      result.expires_at.second.should eq(token.expires_at.second)
+      result.expires_at.zone.should eq(token.expires_at.zone)
+    end
+  end
+end
--- backend/src/token.cr
+++ backend/src/token.cr
@@ -4,44 +4,49 @@ require "base64"
 
 class Token
   include JSON::Serializable
+
+  @[JSON::Field(key: "id")]
+  property id : UUID
+
+  # When this is called through from_json
+  # It rounds the time to the second, and does not carry
+  # over the smaller time measurements
+  @[JSON::Field(key: "expires_at")]
   property expires_at : Time
 
-  def initialize(@owner_id : UUID)
-    @id = UUID.random
-    @expires_at = Time.utc.shift days: 1
-  end
+  getter :id, :expires_at
 
-  def initialize(@owner_id : UUID)
-    @id = UUID.random
-    @expires_at = Time.utc.shift days: 1
-  end
-
-  def self.from_encoded(encoded_token : String)
-    decoded = Base64.decode encoded_token
-    p! typeof(decoded)
-    p! decoded
+  def initialize(@id = UUID.random,
+                 @expires_at = Time.utc.shift days: 1)
   end
 
   def to_json(json : JSON::Builder)
     json.object do
-      json.field "value", self.encode
+      json.field "id", @id.to_s
+      json.field "expires_at", @expires_at
     end
   end
 
   def self.decode(encoded_token : String)
-    Base64.decode encoded_token
+    Base64.decode_string encoded_token
   end
 
   # Takes an encoded token
   def decode(encoded_token : String)
-    Base64.decode encoded_token
+    Base64.decode_string encoded_token
   end
 
   def encode
-    Base64.encode({"id" => @id, "expires_at" => @expires_at, "owner_id" => @owner_id}.to_s)
+    Base64.strict_encode(self.to_json)
   end
 
   def is_valid?
     @expires_at > Time.utc
+  end
+
+  def self.from_encoded(encoded : String)
+    json = Base64.decode_string encoded
+
+    Token.from_json json
   end
 end
